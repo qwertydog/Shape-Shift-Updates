151af65a53ce3a87d3101e84b31db6f19213d926
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-22 21:47:37 +1300

    Consolidate methods to reuse code

--- src/ShapeShifter.php
+++ src/ShapeShifter.php
@@ -4,32 +4,15 @@ namespace SSPro;
 
 class ShapeShifter
 {
-    private $rate;
 
-
-    public function get_BTC_ETH()
+    /**
+     * Gets the Shape Shifter rates from the public API
+     *
+     * Valid coins must be in the BTC_<alt> naming schedule
+     */
+    public function getCoinData($coin)
     {
-        $uri = "https://shapeshift.io/marketinfo/btc_eth";
-        $result = json_decode(file_get_contents($uri), true); // converts it to an array object
-        return $result;
-    }
-
-    public function get_ETH_BTC()
-    {
-        $uri = "https://shapeshift.io/marketinfo/eth_btc";
-        $result = json_decode(file_get_contents($uri), true); // converts it to an array object
-        return $result;
-    }
-
-    public function get_BTC_XMR()
-    {
-        $uri = "https://shapeshift.io/marketinfo/btc_xmr";
-        $result = json_decode(file_get_contents($uri), true); // converts it to an array object
-        return $result;
-    }
-    public function get_BTC_DASH()
-    {
-        $uri = "https://shapeshift.io/marketinfo/btc_dash";
+        $uri = "https://shapeshift.io/marketinfo/$coin";
         $result = json_decode(file_get_contents($uri), true); // converts it to an array object
         return $result;
     }
