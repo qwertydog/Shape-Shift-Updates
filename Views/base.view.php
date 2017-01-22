cbd31f14edebe01532f1b49158b55d5fa636105a
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-22 21:47:09 +1300

    Added styling and removed dropdown

--- Views/base.view.php
+++ Views/base.view.php
@@ -10,24 +10,31 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
             integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
             crossorigin="anonymous"></script>
-    <script src="../Views/js/verify.js"></script>
+    <script src="../Views/js/my.js"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
           integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/bootstrap-select.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/bootstrap-select.min.js"></script>
+
     </head>
     <body>
 
 
+
+
     <!--    Center display for pages-->
         <div class="page-container">
-            <div class="container">
-
-                <div class="col-sm-6 col-md-10" style="padding-top: 20px">
+                <div class="col-xs col-sm-6 col-md-10" style="padding-top: 20px">
                     <?php include($viewName . '.php'); ?>
                 </div>
-            </div>
         </div>
 
     </body>
